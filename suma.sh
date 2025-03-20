39d604411cc9d3585b818c38d6595665a8745617
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-03-20 17:55:29 +0100

    añadir -s

--- suma.sh
+++ suma.sh
@@ -19,3 +19,15 @@ echo "El resultado es : " $?
 
 # result=$(addition $int1 $int2)
 # echo "El resultado es: $result"
+
+
+#Para hacerlo sin ver el numero que se introduce
+#!/bin/bash
+# addition () {
+# sum=$(($1+$2))
+# return $sum
+# }
+# read -p "Ingresa un número: " -s int1
+# read -p "Ingresa un número: " -s int2
+# addition $int1 $int2
+# echo "El resultado es : " $?
